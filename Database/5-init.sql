f04cc86335d8b9c6cc345b4d79fe052bf88a564a
Rafa Pallarés <rafael.pallares@bahiasoftware.es>
2025-10-13 16:25:02 +0200

    Revert "Sync with the other repo and add database split"
    This reverts commit a45c54ffadeccfeabc08beda0f99bbbfac5c66de.

--- Database/5-init.sql
+++
@@ -1,6 +0,0 @@
--- Step 5 ETL initialization check
-
-CREATE TABLE jobs (id VARCHAR(250) PRIMARY KEY, filename VARCHAR(250), step  NUMERIC);
-
-CREATE TABLE init_complete (status BOOLEAN);
-INSERT INTO init_complete VALUES (TRUE);
