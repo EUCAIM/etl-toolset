2a01b240cab2b863bfd8878954e8c90f74a4bc29
vsonpom <victor.sonora@bahiasoftware.es>
2026-04-09 10:26:56 +0200

    ci

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -13,69 +13,6 @@ OUTPUT_DIR="output_data"
 POSTGRES_CONTAINER=$(docker compose ps -q nifi-postgres)
 
 
-### validations for clinical data
-rm -f $OUTPUT_DIR/*.csv
-
-TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_Breast_Cancer3_sample.csv"
-NUMBER_OF_PATIENTS=4
-cp "$TEST_CSV" "$INPUT_DIR/clinical_data/"
-echo "Copied clinical data sample file to $INPUT_DIR"
-
-MAX_RETRIES=130   
-SLEEP_SEC=5      
-COUNT=0
-until find "$OUTPUT_DIR" -maxdepth 1 -type f -name "*.csv" | grep -q .; do
-  if [ $COUNT -ge $MAX_RETRIES ]; then
-    echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
-    exit 1
-  fi
-
-  echo "Still waiting..."
-  sleep $SLEEP_SEC
-  COUNT=$((COUNT+1))
-done
-
-echo "Output detected!"
-
-echo "Files generated:"
-ls -l "$OUTPUT_DIR"
-
-echo "Validating rows number for clinical data in output files..."
-TOTAL_ROWS=0
-for f in $OUTPUT_DIR/patient*.csv; do
-  ROWS=$(($(wc -l < "$f") - 1))
-  TOTAL_ROWS=$((TOTAL_ROWS + ROWS))
-done
-
-echo "Number of output rows in patient csv files: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
-
-if [ "$TOTAL_ROWS" -eq 0 ]; then
-  echo "❌ Not processed data"
-  #exit 1
-fi
-
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
-  echo "❌ Output seems not correct"
-  #exit 1
-fi
-
-echo "Validating rows numbers for clinical data in staging database..."
-TOTAL_ROWS=0
-TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_ingestion.CancerPatient;" | xargs)
-
-echo "Number of output rows in eucaim_cdm_ingestion.ImageStudy table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
-
-if [ "$TOTAL_ROWS" -eq 0 ]; then
-  echo "❌ Not processed data"
-  exit 1
-fi
-
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
-  echo "❌ Output seems not correct"
-  exit 1
-fi
-
-
 ### validations for imaging metadata
 rm -f $OUTPUT_DIR/*.csv
 
@@ -135,6 +72,70 @@ if [ "$TOTAL_ROWS" -eq 0 ]; then
 fi
 
 if [ "$TOTAL_ROWS" -ne $NUMBER_OF_STUDIES ]; then
+  echo "❌ Output seems not correct"
+  exit 1
+fi
+
+
+### validations for clinical data
+rm -f $OUTPUT_DIR/*.csv
+
+TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_Breast_Cancer3_sample.csv"
+NUMBER_OF_PATIENTS=4
+cp "$TEST_CSV" "$INPUT_DIR/clinical_data/"
+echo "Copied clinical data sample file to $INPUT_DIR"
+
+MAX_RETRIES=70   
+SLEEP_SEC=5      
+COUNT=0
+until find "$OUTPUT_DIR" -maxdepth 1 -type f -name "*.csv" | grep -q .; do
+  if [ $COUNT -ge $MAX_RETRIES ]; then
+    echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
+    ls -l "$OUTPUT_DIR"
+    exit 1
+  fi
+
+  echo "Still waiting..."
+  sleep $SLEEP_SEC
+  COUNT=$((COUNT+1))
+done
+
+echo "Output detected!"
+
+echo "Files generated:"
+ls -l "$OUTPUT_DIR"
+
+echo "Validating rows number for clinical data in output files..."
+TOTAL_ROWS=0
+for f in $OUTPUT_DIR/patient*.csv; do
+  ROWS=$(($(wc -l < "$f") - 1))
+  TOTAL_ROWS=$((TOTAL_ROWS + ROWS))
+done
+
+echo "Number of output rows in patient csv files: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
+
+if [ "$TOTAL_ROWS" -eq 0 ]; then
+  echo "❌ Not processed data"
+  #exit 1
+fi
+
+if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
+  echo "❌ Output seems not correct"
+  #exit 1
+fi
+
+echo "Validating rows numbers for clinical data in staging database..."
+TOTAL_ROWS=0
+TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_ingestion.CancerPatient;" | xargs)
+
+echo "Number of output rows in eucaim_cdm_ingestion.ImageStudy table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
+
+if [ "$TOTAL_ROWS" -eq 0 ]; then
+  echo "❌ Not processed data"
+  exit 1
+fi
+
+if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
   echo "❌ Output seems not correct"
   exit 1
 fi
