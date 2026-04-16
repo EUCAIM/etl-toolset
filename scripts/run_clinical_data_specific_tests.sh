65bde92cb1fc8d4488e66d39abf1369d210904e9
vsonpom <victor.sonora@bahiasoftware.es>
2026-04-16 14:29:10 +0200

    Refactoring ci tests to prepare generalization

---
+++ scripts/run_clinical_data_specific_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "==== RUNNING TEST: start clinical data specific tests ===="
+echo "Nothing more to do, or say"
+echo "==== RUNNING TEST: close clinical data specific tests ===="
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,178 +6,197 @@
 
 
 set -e  # fail if any command fails
-echo "==== RUNNING TEST: processing imaging metadata sample ===="
+echo "==== RUNNING TEST: start main ===="
 
+
+### definitions and parameters
 INPUT_DIR="input_data"
 OUTPUT_DIR="output_data"
 POSTGRES_CONTAINER=$(docker compose ps -q nifi-postgres)
 
-
-### validations for imaging metadata
-rm -f $OUTPUT_DIR/*.csv
-
-TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_AUTH_dcm_series_metadata_breast_few_studies.csv"
+CLINICAL_DATA_TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_Breast_Cancer3_sample.csv"
+NUMBER_OF_PATIENTS=4
+IMAGE_METADATA_TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_AUTH_dcm_series_metadata_breast_few_studies.csv"
 NUMBER_OF_STUDIES=4
-cp "$TEST_CSV" "$INPUT_DIR/image_metadata/"
-echo "Copied imaging metadata sample file to $INPUT_DIR"
+IMAGING_TIMEPOINTS_TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_AUTH_imaging_timepoints_dcm_studies_metadata_breast.csv"
+
+CLINICAL_DATA_EXTRA_TEST_SCRIPT="scripts/run_clinical_data_specific_tests.sh"
 
 MAX_RETRIES=60   
 SLEEP_SEC=5      
 COUNT=0
-until find "$OUTPUT_DIR" -maxdepth 1 -type f -name "*.csv" | grep -q .; do
-  if [ $COUNT -ge $MAX_RETRIES ]; then
-    echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
-    docker logs nifi-tdc
-    docker logs nifi
-    exit 1
-  fi
-
-  echo "Still waiting..."
-  sleep $SLEEP_SEC
-  COUNT=$((COUNT+1))
-done
-
-echo "Output detected!"
-
-echo "Files generated:"
-ls -l "$OUTPUT_DIR"
-
-echo "Validating rows number for imaging metadata in output files..."
-TOTAL_ROWS=0
-for f in $OUTPUT_DIR/image_study*.csv; do
-  ROWS=$(($(wc -l < "$f") - 1))
-  TOTAL_ROWS=$((TOTAL_ROWS + ROWS))
-done
-
-echo "Number of output rows in image_study csv files: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_STUDIES)"
-
-if [ "$TOTAL_ROWS" -eq 0 ]; then
-  echo "❌ Not processed data"
-  #exit 1
-fi
-
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_STUDIES ]; then
-  echo "❌ Output seems not correct"
-  #exit 1
-fi
-
-
-echo "Validating rows number for imaging metadata in staging database..."
-TOTAL_ROWS=0
-TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_ingestion.ImageStudy;" | xargs)
-
-echo "Number of output rows in eucaim_cdm_ingestion.ImageStudy table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_STUDIES)"
-
-if [ "$TOTAL_ROWS" -eq 0 ]; then
-  echo "❌ Not processed data"
-  exit 1
-fi
-
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_STUDIES ]; then
-  echo "❌ Output seems not correct"
-  exit 1
-fi
 
 
 ### validations for clinical data
-rm -f $OUTPUT_DIR/*.csv
+procesar_pipeline_clinical_data() {
+  rm -f $OUTPUT_DIR/*.csv
+  cp "$CLINICAL_DATA_TEST_CSV" "$INPUT_DIR/clinical_data/"
+  echo "Copied clinical data sample file to $INPUT_DIR"
 
-TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_Breast_Cancer3_sample.csv"
-NUMBER_OF_PATIENTS=4
-cp "$TEST_CSV" "$INPUT_DIR/clinical_data/"
-echo "Copied clinical data sample file to $INPUT_DIR"
+  until find "$OUTPUT_DIR" -maxdepth 1 -type f -name "*.csv" | grep -q .; do
+    if [ $COUNT -ge $MAX_RETRIES ]; then
+      echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
+      docker logs nifi-tdc
+      docker logs nifi
+      exit 1
+    fi
 
-MAX_RETRIES=70   
-SLEEP_SEC=5      
-COUNT=0
-until find "$OUTPUT_DIR" -maxdepth 1 -type f -name "*.csv" | grep -q .; do
-  if [ $COUNT -ge $MAX_RETRIES ]; then
-    echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
-    docker logs nifi-tdc
-    docker logs nifi
+    echo "Still waiting..."
+    sleep $SLEEP_SEC
+    COUNT=$((COUNT+1))
+  done
+
+  echo "Output detected!"
+  echo "Files generated:"
+  ls -l "$OUTPUT_DIR"
+
+  echo "Validating rows number for clinical data in output files..."
+  TOTAL_ROWS=0
+  for f in $OUTPUT_DIR/patient*.csv; do
+    ROWS=$(($(wc -l < "$f") - 1))
+    TOTAL_ROWS=$((TOTAL_ROWS + ROWS))
+  done
+
+  echo "Number of output rows in patient csv files: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
+
+  if [ "$TOTAL_ROWS" -eq 0 ]; then
+    echo "❌ Not processed data"
     exit 1
   fi
 
-  echo "Still waiting..."
-  sleep $SLEEP_SEC
-  COUNT=$((COUNT+1))
-done
-
-echo "Output detected!"
-
-echo "Files generated:"
-ls -l "$OUTPUT_DIR"
-
-echo "Validating rows number for clinical data in output files..."
-TOTAL_ROWS=0
-for f in $OUTPUT_DIR/patient*.csv; do
-  ROWS=$(($(wc -l < "$f") - 1))
-  TOTAL_ROWS=$((TOTAL_ROWS + ROWS))
-done
-
-echo "Number of output rows in patient csv files: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
-
-if [ "$TOTAL_ROWS" -eq 0 ]; then
-  echo "❌ Not processed data"
-  #exit 1
-fi
-
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
-  echo "❌ Output seems not correct"
-  #exit 1
-fi
-
-echo "Validating rows numbers for clinical data in staging database..."
-TOTAL_ROWS=0
-TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_ingestion.CancerPatient;" | xargs)
-
-echo "Number of output rows in eucaim_cdm_ingestion.ImageStudy table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
-
-if [ "$TOTAL_ROWS" -eq 0 ]; then
-  echo "❌ Not processed data"
-  exit 1
-fi
-
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
-  echo "❌ Output seems not correct"
-  exit 1
-fi
-
-
-### validations for imaging timepoints
-rm -f $OUTPUT_DIR/*.csv
-
-TEST_CSV="sample_data/4fcdd34b95f8eed2a3d07291e4c2173e_AUTH_imaging_timepoints_dcm_studies_metadata_breast.csv"
-NUMBER_OF_PATIENTS=4
-cp "$TEST_CSV" "$INPUT_DIR/image_timepoints/"
-echo "Copied imaging timepoints sample file to $INPUT_DIR"
-
-
-echo "Validating rows numbers in output database..."
-MAX_RETRIES=40  
-SLEEP_SEC=5      
-COUNT=0
-TOTAL_ROWS=0
-
-until [ "$TOTAL_ROWS" -ne 0 ]; do
-  if [ $COUNT -ge $MAX_RETRIES ]; then
-    echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
-    docker logs nifi-tdc
-    docker logs nifi
+  if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
+    echo "❌ Output seems not correct"
     exit 1
   fi
-  TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_output.patient;" | xargs)
-  echo "Still waiting..."
-  sleep $SLEEP_SEC
-  COUNT=$((COUNT+1))
-done
 
-echo "Number of output rows in eucaim_cdm_output.patient table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
+  echo "Validating rows numbers for clinical data in staging database..."
+  TOTAL_ROWS=0
+  TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_ingestion.CancerPatient;" | xargs)
 
-if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
-  echo "❌ Output seems not correct"
-  exit 1
+  echo "Number of output rows in eucaim_cdm_ingestion.CancerPatient table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
+
+  if [ "$TOTAL_ROWS" -eq 0 ]; then
+    echo "❌ Not processed data"
+    exit 1
+  fi
+
+  if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
+    echo "❌ Output seems not correct"
+    exit 1
+  fi
+}
+  
+if [ -f $CLINICAL_DATA_TEST_CSV ]; then
+  procesar_pipeline_clinical_data
+
+  if [ -f $CLINICAL_DATA_EXTRA_TEST_SCRIPT ]; then
+    echo "Detected additional clinical data tests on: $CLINICAL_DATA_EXTRA_TEST_SCRIPT"
+    source $CLINICAL_DATA_EXTRA_TEST_SCRIPT
+  fi
 fi
 
+
+
+### validations for imaging metadata pipelines
+procesar_pipeline_imaging_metadata() {
+  rm -f $OUTPUT_DIR/*.csv
+  cp "$IMAGE_METADATA_TEST_CSV" "$INPUT_DIR/image_metadata/"
+  echo "Copied imaging metadata sample file to $INPUT_DIR"
+
+  until find "$OUTPUT_DIR" -maxdepth 1 -type f -name "*.csv" | grep -q .; do
+    if [ $COUNT -ge $MAX_RETRIES ]; then
+      echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
+      docker logs nifi-tdc
+      docker logs nifi
+      exit 1
+    fi
+
+    echo "Still waiting..."
+    sleep $SLEEP_SEC
+    COUNT=$((COUNT+1))
+  done
+
+  echo "Output detected!"
+  echo "Files generated:"
+  ls -l "$OUTPUT_DIR"
+
+  echo "Validating rows number for imaging metadata in output files..."
+  TOTAL_ROWS=0
+  for f in $OUTPUT_DIR/image_study*.csv; do
+    ROWS=$(($(wc -l < "$f") - 1))
+    TOTAL_ROWS=$((TOTAL_ROWS + ROWS))
+  done
+
+  echo "Number of output rows in image_study csv files: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_STUDIES)"
+
+  if [ "$TOTAL_ROWS" -eq 0 ]; then
+    echo "❌ Not processed data"
+    exit 1
+  fi
+
+  if [ "$TOTAL_ROWS" -ne $NUMBER_OF_STUDIES ]; then
+    echo "❌ Output seems not correct"
+    exit 1
+  fi
+
+  echo "Validating rows number for imaging metadata in staging database..."
+  TOTAL_ROWS=0
+  TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_ingestion.ImageStudy;" | xargs)
+
+  echo "Number of output rows in eucaim_cdm_ingestion.ImageStudy table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_STUDIES)"
+
+  if [ "$TOTAL_ROWS" -eq 0 ]; then
+    echo "❌ Not processed data"
+    exit 1
+  fi
+
+  if [ "$TOTAL_ROWS" -ne $NUMBER_OF_STUDIES ]; then
+    echo "❌ Output seems not correct"
+    exit 1
+  fi
+}
+
+if [ -f $IMAGE_METADATA_TEST_CSV ]; then
+  procesar_pipeline_imaging_metadata
+fi
+
+
+### validations for imaging timepoints pipelines
+procesar_pipeline_imaging_timepoints() {
+  rm -f $OUTPUT_DIR/*.csv
+  cp "$IMAGING_TIMEPOINTS_TEST_CSV" "$INPUT_DIR/image_timepoints/"
+  echo "Copied imaging timepoints sample file to $INPUT_DIR"
+
+  echo "Validating rows numbers in output database..."
+
+  TOTAL_ROWS=0
+  until [ "$TOTAL_ROWS" -ne 0 ]; do
+    if [ $COUNT -ge $MAX_RETRIES ]; then
+      echo "Timeout: No output files detected after $((MAX_RETRIES*SLEEP_SEC)) seconds."
+      docker logs nifi-tdc
+      docker logs nifi
+      exit 1
+    fi
+    TOTAL_ROWS=$(docker exec $POSTGRES_CONTAINER psql -U postgres -d eucaim-etl-db -t -c "SELECT COUNT(*) FROM eucaim_cdm_output.patient;" | xargs)
+    echo "Still waiting..."
+    sleep $SLEEP_SEC
+    COUNT=$((COUNT+1))
+  done
+
+  echo "Number of output rows in eucaim_cdm_output.patient table: $TOTAL_ROWS  (Expected rows: $NUMBER_OF_PATIENTS)"
+
+  if [ "$TOTAL_ROWS" -ne $NUMBER_OF_PATIENTS ]; then
+    echo "❌ Output seems not correct"
+    exit 1
+  fi
+}
+
+if [ -f $IMAGING_TIMEPOINTS_TEST_CSV ]; then
+  procesar_pipeline_imaging_timepoints
+fi
+
+
+### closing tests
 echo "Test PASSED"
+echo "==== RUNNING TEST: close main ===="
 exit 0
